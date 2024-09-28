f1e87f98e78841948d52acc0d7aac164a226625d
Kelly Ton <kthton@gmail.com>
2024-09-28 18:30:01 -0400

    create navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Scroll -> navbar becomes more opaque
+
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
     var scrollPosition = window.scrollY || window.pageYOffset;
@@ -8,3 +10,21 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled'); // Remove background when above threshold
     }
 });
+
+
+// hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-links");
+const navbar = document.querySelector(".navbar"); // Select the navbar
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    navbar.classList.toggle("active"); // Toggle the active class on the navbar
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    navbar.classList.remove("active"); // Remove the active class when a link is clicked
+}));
